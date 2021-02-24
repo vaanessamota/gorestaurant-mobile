ce59aad604e21a008fed5a1a2f1797a4f16a59d9
Vanessa <vanessaoliveira2706@gmail.com>
2021-02-23 21:14:44 -0300

    challenge done

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,8 +32,17 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites', {
+      });
+
+      setFavorites(
+        response.data.map((favorite: Food)=> ({
+          ...favorite,
+          formattedPrice: formatValue(favorite.price),
+        }))
+      )
     }
+
 
     loadFavorites();
   }, []);
